b7a649e1030e67de382411ea39cdeac8f59e7625
Volodymyr Novak <novakvova@gmail.com>
2020-12-16 15:49:01 +0200

    show image is good

--- resources/views/post/details.blade.php
+++ resources/views/post/details.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<img src="<?php echo $post->url; ?>" class="card-img-top" alt="...">
+
+<img src="<?php echo $post->url; ?>" width="150"  alt="">
 <h3>{{ $post->title }}</h3>
+
 
 @endsection
